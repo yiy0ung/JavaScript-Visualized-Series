dc3edf54d432299a006d5d246e11d0c7d0a8238a
wlsdud2194 <38432821+wlsdud2194@users.noreply.github.com>
2020-02-03 20:34:05 +0900

    ADD example code

---
+++ decorator.js
@@ -0,0 +1,34 @@
+"Example Code";
+
+/** Part 1 */
+const foo = () => console.log("First"); // The first item in call stack
+
+// The callback function go to Web API. After waiting 500ms, 
+// It is gonna queue and will be the third item in call stack.
+const bar = () => setTimeout(() => console.log("Second"), 500);
+const baz = () => console.log("Third"); // The second item in call stack
+
+bar();
+foo();
+baz();
+// Output:
+// First
+// Third
+// Second
+
+
+/** Part 2 */
+Hosting(); // call function
+console.log(varHosting);
+console.log(constHosting);
+
+var varHosting = 20;
+const constHosting = 'Hello Universe?';
+function Hosting() {
+  console.log('It\'s just example code for looking beautiful in Github Repository');
+}
+
+// Output:
+// It's just example code for looking beautiful in Github Repository
+// undefinded
+// Uncaught ReferenceErrer: Cannot access 'constHosting' before initialization <-- Error
