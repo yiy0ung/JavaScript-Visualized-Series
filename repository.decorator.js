2f9b5e31fbeb6ac56575b9209aeb4708c42db3bd
wlsdud2194 <38432821+wlsdud2194@users.noreply.github.com>
2020-02-28 22:56:51 +0900

    ADD example code of part 3

--- repository.decorator.js
+++ repository.decorator.js
@@ -1,6 +1,6 @@
 "Example Code"
 
-/** Part 1 */
+/** Part 1 ------------------------ */
 const foo = () => console.log("First"); // The first item in call stack
 
 // The callback function go to Web API. After waiting 500ms, 
@@ -11,16 +11,17 @@ const baz = () => console.log("Third"); // The second item in call stack
 bar();
 foo();
 baz();
-// Output:
-// First
-// Third
-// Second
+
+  // Output:
+  // First
+  // Third
+  // Second
 
 
-/** Part 2 */
-Hosting(); // call function
-console.log(varHosting);
-console.log(constHosting);
+/** Part 2 ------------------------ */
+Hosting(); // Function is stored with a reference to the entire function
+console.log(varHosting); // Variable declared var keyword is initialized "undefined" type
+console.log(constHosting); // Variable declared let or const keyword is not initialized 
 
 var varHosting = 20;
 const constHosting = 'Hello Universe?';
@@ -28,7 +29,31 @@ function Hosting() {
   console.log('It\'s just example code for looking beautiful in Github Repository');
 }
 
-// Output:
-// It's just example code for looking beautiful in Github Repository
-// undefinded
-// Uncaught ReferenceErrer: Cannot access 'constHosting' before initialization <-- Error
+  // Output:
+  // It's just example code for looking beautiful in Github Repository
+  // undefinded
+  // Uncaught ReferenceErrer: Cannot access 'constHosting' before initialization <-- Error
+
+
+/** Part 3 ------------------------ */
+const name = 'Jane';
+const age = 19;
+const city = 'Seoul, South Korea';
+
+function getPersonInfo() {
+  const name = 'Alex';
+  const age = 25;
+  const gender = 'man';
+
+  return `${name} is ${age}, lives in ${city}, and is the ${gender}`; // Outter scope can be referenced in the inner scope
+}
+
+console.log(getPersonInfo);
+console.log(`${name} is ${age}, lives in ${city}, and is the ${gender}`); // Cannot reference the variable, gender in the inner scope
+
+  // Output:
+  // Alex is 25, lives in Seoul, South Korea, and is the man
+  // Uncaught ReferenceError: gender is not defined <-- Error
+
+
+
